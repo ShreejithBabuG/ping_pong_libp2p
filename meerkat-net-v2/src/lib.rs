e3f1f37aae1b0e8944fa324662930baebe6bfa48
Shreejith Babu G <shreejith@Shreejiths-MacBook-Air.local>
2026-02-26 06:37:21 +0530

    Extract protocol.rs, fix ip6 translation

--- meerkat-net-v2/src/lib.rs
+++ meerkat-net-v2/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod types;
 pub mod messages;
 pub mod actor;
+pub mod protocol;
 
 pub use types::*;
 pub use messages::*;
 pub use actor::NetworkActor;
+pub use protocol::{MEERKAT_PROTOCOL, send_message, recv_message};
